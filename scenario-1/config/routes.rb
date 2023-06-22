c80e6a8b6f0823efa7b04a3cbf4cf9c02a26ac25
David Moulton <dave@evoca.tv>
2023-06-22 17:01:50 -0600

    added convenience routes

--- scenario-1/config/routes.rb
+++ scenario-1/config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   get 'content/index'
+  get 'content', to: "content#index"
   post 'content/save'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "content#index"
 end
